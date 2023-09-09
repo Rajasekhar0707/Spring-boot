72096399677defc0a339997a5410b9a7d3b307eb
Rajasekhar0707 <53522979+Rajasekhar0707@users.noreply.github.com>
2023-09-09 13:22:47 +0530

    Update RestControll.java

--- src/main/java/com/example/demo/RestControll.java
+++ src/main/java/com/example/demo/RestControll.java
@@ -1,1 +1,12 @@
 
+package com.example.demo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControll {
+    @PostMapping("/employees")
+  String newEmployee() {
+    return "Testing the Application";
+  }
+}
